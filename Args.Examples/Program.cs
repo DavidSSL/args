681c3148796119c667069a020ff81419a38929b1
David Siew <davidsiew@outlook.com>
2013-05-02 10:35:36 +0100

    - Add example project to show how to use certain features.

---
+++ Args.Examples/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Args.Examples
+{
+    class Program
+    {
+        [ArgsModel(SwitchDelimiter = "-")]
+        public class CommandOption
+        {
+            public Uri OutputFilePath { get; set; }
+            public string PatientId { get; set; }
+
+            [ArgsMemberSwitch("d")]
+            public bool IsDatabase { get; set; }
+            [ArgsMemberSwitch("x")]
+            public bool IsXml { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            var command = Configuration.Configure<CommandOption>().CreateAndBind(args);
+
+            Console.WriteLine(@"OutputFilePath: {0} 
+PatientId: {1} 
+IsDatabase: {2} 
+IsXml: {3}"
+                , command.OutputFilePath
+                , command.PatientId
+                , command.IsDatabase
+                , command.IsXml);
+
+            Console.ReadLine();
+        }
+    }
+}
